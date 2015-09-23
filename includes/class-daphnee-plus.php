5bfd0168cf838ee49e4216f59e00fcb1426224eb
Aristeides Stathopoulos <aristath@gmail.com>
2015-09-23 18:43:19 +0300

    adding basic TODOs

--- includes/class-daphnee-plus.php
+++ includes/class-daphnee-plus.php
@@ -1,5 +1,13 @@
 <?php
 
 class Daphnee_Plus {
-    
+
+    // TODO: Add extra partial templates folder
+
+    // TODO: Add extra typography options for headers (h1 - h6)
+
+    // TODO: Add extra header templates
+
+    // TODO: Add option to change the footer text (copyright)
+
 }
