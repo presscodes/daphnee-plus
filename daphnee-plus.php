b438f3cfe3380b7514aaf92fa5d332897f2a332e
Aristeides Stathopoulos <aristath@gmail.com>
2015-09-23 18:25:03 +0300

    add plugin file headers

---
+++ daphnee-plus.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name:   Daphnee Plus
+ * Plugin URI:    http://press.codes
+ * Description:   Premium plugin, adding extra features to the Daphnee theme
+ * Author:        the PressCodes team
+ * Author URI:    http://press.codes
+ * Version:       0.1
+ * Text Domain:   daphnee-plus
+ */
